bee9de72c39ce87696f1348233819e74d7bcb25e
gus <gustavares.dev@gmail.com>
2023-08-21 18:44:25 -0300

    chore: removing function from songOperations

--- data/song.mjs
+++ data/song.mjs
@@ -4,12 +4,6 @@ const songOperations = {
   getSongsByUserId: async (userId) => {
     return await knex('songs').where({ 'user_id': userId });
   },
-  getSongsByPlaylistId: async (playlistId) => {
-    return await knex('playlist_songs')
-      .select('songs.*')
-      .join('songs', 'songs.id', 'playlist_songs.song_id')
-      .where({ 'playlist_id ': playlistId })
-  },
   createSong: async (userId, name) => {
     const [newSong] = await knex('songs')
       .returning('*')
